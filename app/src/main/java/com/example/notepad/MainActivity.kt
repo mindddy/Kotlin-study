cc5f6fc30afaa2c4e0b1367d7872df835d9028f7
Minji Kang <1117minsky@hufs.ac.kr>
2023-05-12 22:40:58 +0900

    [notpad]viewbinding 설정

--- app/src/main/java/com/example/notepad/MainActivity.kt
+++ app/src/main/java/com/example/notepad/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notepad.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
--- app/src/main/java/com/example/notepad/SubActivity.kt
+++ app/src/main/java/com/example/notepad/SubActivity.kt
@@ -2,10 +2,16 @@ package com.example.notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notepad.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySubBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sub)
+        binding = ActivitySubBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
